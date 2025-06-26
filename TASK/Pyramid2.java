9ce04480e585fa173de8a6aa98823ffe8f1f946d
Subha8825 <146204172+Subha8825@users.noreply.github.com>
2025-06-26 15:05:24 +0530

    Pyramid num pattern

---
+++ TASK/Pyramid2.java
@@ -0,0 +1,22 @@
+package com.index;
+
+import java.util.Scanner;
+
+public class Pyramid2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner obj=new Scanner(System.in);
+		int row=obj.nextInt();
+		for(int i=1;i<=row;i++) {
+			for(int j=1;j<=row-i;j++) {
+				System.out.print(" ");			}
+			for(int k=1;k<=i;k++) {
+				System.out.print(k+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
